edb446f160f9b16733af5f86fb76dd71c1ae5747
Maribel <itirados@paradigmadigital.com>
2020-11-04 12:36:26 +0100

    Tests implementation.

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ The target coordinates are validated.
 The game checks if the move introduced by console can be done. 
 The board is updated with the move. The turn is changed and the number of captured pieces are showed.
 Return the player's last captured piece.
+Some test implemented.
 
 To execute the program:
 ```
--- tests/test_board.py
+++ tests/test_board.py
@@ -9,9 +9,10 @@ from pieces.king import King
 from pieces.rook import Rook
 from pieces.bishop import Bishop
 from pieces.pawn import Pawn
+from player import Player
+from pieces.piece import Piece
+from shogi_constants import PLAYER_1 as p1, PLAYER_2 as p2
 
-def test_always_passes():
-    assert True
 
 def test_silver_movement():
     from_x = 4
@@ -20,15 +21,15 @@ def test_silver_movement():
     to_y = 3
     board = Board()
     board.state = [
-        [Lance("v"),Knight("v"),None,Gold("v"),King("v"),Gold("v"),Silver("v"),Knight("v"),Lance("v")],
-            [None,Rook("v"),None,None,None,None,None,Bishop("v"),None],
-            [Pawn("v"),Pawn("v"),Pawn("v"),Pawn("v"),Pawn("v"),Pawn("v"),Pawn("v"),Pawn("v"),Pawn("v")],
+        [Lance(p1),Knight(p1),None,Gold(p1),King(p1),Gold(p1),Silver(p1),Knight(p1),Lance(p1)],
+            [None,Rook(p1),None,None,None,None,None,Bishop(p1),None],
+            [Pawn(p1),Pawn(p1),Pawn(p1),Pawn(p1),Pawn(p1),Pawn(p1),Pawn(p1),Pawn(p1),Pawn(p1)],
             [None,None,None,None,None,None,None,None,None],
-            [None,None,None,None,Silver("v"),None,None,None,None],
+            [None,None,None,None,Silver(p1),None,None,None,None],
             [None,None,None,None,None,None,None,None,None],
-            [Pawn("^"),Pawn("^"),Pawn("^"),Pawn("^"),Pawn("^"),Pawn("^"),Pawn("^"),Pawn("^"),Pawn("^")],
-            [None,Bishop("^"),None,None,None,None,None,Rook("^"),None],
-            [Lance("^"),Knight("^"),Silver("^"),Gold("^"),King("^"),Gold("^"),Silver("^"),Knight("^"),Lance("^")],
+            [Pawn(p2),Pawn(p2),Pawn(p2),Pawn(p2),Pawn(p2),Pawn(p2),Pawn(p2),Pawn(p2),Pawn(p2)],
+            [None,Bishop(p2),None,None,None,None,None,Rook(p2),None],
+            [Lance(p2),Knight(p2),Silver(p2),Gold(p2),King(p2),Gold(p2),Silver(p2),Knight(p2),Lance(p2)],
     ]
     selected_piece = board.state[from_x][from_y]
     # If the target position (to_x_y) is on the board, we check if the selected piece can make the move
@@ -37,6 +38,23 @@ def test_silver_movement():
         if (selected_piece.correct_move(from_x, from_y, to_x, to_y)):
             board.state[from_x][from_y] = None
             board.state[to_x][to_y] = selected_piece
-    # La pieza de destino tiene que ser la misma que la de origen
+    # target piece must be equal to selected piece
     assert board.state[5][3].piece_type == "S"
    
+def test_change_turn():
+    player = Player()
+    player.current_player = p1
+    player.change_turn()
+    assert player.current_player == p2
+
+def test_on_board():
+    from_x = 4
+    from_y = 0
+    to_x = 0
+    to_y = 5
+    piece = Piece(p1)
+    is_on_board = piece.on_board(from_x, from_y, to_x, to_y)
+    assert is_on_board == True
+
+
+
